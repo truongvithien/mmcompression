5d786c1c1d6dcb59f9eabd881e07cf256a7ddaab
thientv <thientv.cs@gmail.com>
2018-01-11 00:10:51 +0700

    remove imgs

--- arithmetic.py
+++ arithmetic.py
@@ -1,7 +1,14 @@
 import os
 
-def compress(source):
+def init_probability_table(source):
+
     return 0
+
+def compress(source):
+    source_content = source.read()
+    
+    prob_table = init_probability_table(source_content)
+    return 0, prob_table
 
 def decompress(code):
     return 0
